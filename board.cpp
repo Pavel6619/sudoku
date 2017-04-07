d3e6fbbb6f91fda8e6f37a28b24fd661471b85f1
Daniel Gardner <Daniel Gardner>
2017-04-06 21:48:06 -0400

    Added logic to improve recursive calls, as well as correctly solve the puzzle

--- board.cpp
+++ board.cpp
@@ -45,10 +45,6 @@ void board::clear()
 		}
 }
 
-void board::resetCell(int row, int col){
-	setCell(row, col, Blank);
-}
-
 bool board::isSolved(){
 	for(int i = 1; i <= BoardSize; i++){
 		for(int j = 1; j <= BoardSize; j++){
@@ -75,9 +71,9 @@ void board::initialize(ifstream &fin)
 			// If the read char is not Blank
 			if (ch != '.')
 				setCell(i, j, ch - '0');   // Convert char to int
+				//updateConflicts(i, j, value[i][j]);
+			
 		}
-
-	updateConflicts();
 }
 
 int squareNumber(int i, int j)
@@ -90,18 +86,20 @@ int squareNumber(int i, int j)
 	return SquareSize * ((i - 1) / SquareSize) + (j - 1) / SquareSize + 1;
 }
 
-void board::updateConflicts()
+void board::updateConflicts(int i, int j, ValueType val)
 {
-	for (int i = 1; i <= BoardSize; i++)
-		for (int j = 1; j <= BoardSize; j++)
-		{
 			if (!isBlank(i, j))
 			{
-				row_conf[i][value[i][j]] = true;
-				col_conf[j][value[i][j]] = true;
-				square_conf[squareNumber(i, j)][value[i][j]] = true;
+				row_conf[i][val] = true;
+				col_conf[j][val] = true;
+				square_conf[squareNumber(i, j)][val] = true;
 			}
-		}
+			else
+			{
+				row_conf[i][val] = false;
+				col_conf[j][val] = false;
+				square_conf[squareNumber(i, j)][val] = false;
+			}
 }
 
 void board::printConflicts()
@@ -162,13 +160,18 @@ void board::setCell(int i, int j, int val)
 		if (isBlank(i, j))
 		{
 			value[i][j] = val;
-			updateConflicts();
+			updateConflicts(i, j, val);
 		}
 		else
 			cout << "Cell is not blank" << endl;
 	}
 	else
 		throw rangeError("bad value in setCell");
+}
+
+void board::resetCell(int row, int col, ValueType val) {
+	value[row][col] = Blank;
+	updateConflicts(row, col, val);
 }
 
 bool board::isBlank(int i, int j)
@@ -178,6 +181,12 @@ bool board::isBlank(int i, int j)
 		throw rangeError("bad value in setCell");
 
 	return (getCell(i, j) == Blank);
+}
+
+bool board::isLegal(int i, int j, ValueType val)
+{
+	int sq = squareNumber(i, j);
+	return !(row_conf[i][val] || col_conf[j][val] || square_conf[sq][val]);
 }
 
 void board::print()
@@ -211,6 +220,91 @@ void board::print()
 		cout << "---";
 	cout << "-";
 	cout << endl;
+}
+
+void board::findMostConstrainedBlankCell(int& i, int& j)
+{
+	int most_constrained_sum = 0;
+	int gen_sum = 0;
+	for (int k = 1; k <= BoardSize; k++)
+	{
+		for (int m = 1; m <= BoardSize; m++)
+		{
+			if (isBlank(k, m))
+			{
+				for (int n = 1; n <= BoardSize; n++)
+				{
+					//Increment the general sum for a given cell if there is a conflict for this digit
+					gen_sum += (row_conf[k][n] || col_conf[m][n] || square_conf[squareNumber(k, m)][n]);
+				}
+				if (gen_sum > most_constrained_sum)
+				{
+					most_constrained_sum = gen_sum;
+					i = k;
+					j = m;
+				}
+				gen_sum = 0;
+			}
+		}
+	}
+}
+
+void board::findNextBlankCell(int& i, int& j)
+{
+	for (int k = 1; k <= BoardSize; k++)
+	{
+		for (int m = 1; m <= BoardSize; m++)
+		{
+			if (isBlank(k, m))
+			{
+				i = k;
+				j = m;
+				return;
+			}
+		}
+	}
+}
+
+matrix<ValueType> board::getValue()
+{
+	return value;
+}
+
+void board::incrementRecursiveCalls()
+{
+	numRecursiveCalls++;
+}
+
+long board::getRecursiveCalls()
+{
+	return numRecursiveCalls;
+}
+
+bool solve(board& b)
+{
+	b.incrementRecursiveCalls();
+	int i, j;
+	if (b.isSolved())
+	{
+		b.print();
+		cout << "Solved" << endl;
+		return true;
+	}
+	else
+	{
+		//b.findNextBlankCell(i, j);
+		b.findMostConstrainedBlankCell(i, j);
+		for (int k = 1; k <= 9; k++)
+		{
+			if (b.isLegal(i, j, k))
+			{
+				b.setCell(i, j, k);
+				if (!solve(b))
+					b.resetCell(i, j, k);
+			}
+		}
+		return false;
+	}
 }
 
 int main()
@@ -258,22 +352,15 @@ int main()
 	try
 	{
 		board b1(SquareSize);
-
+		int i = 1, j = 1;
 		while (fin && fin.peek() != 'Z')
 		{
 			b1.initialize(fin);
 			b1.print();
-			cout << "Conflicts: " << endl;
-			b1.printConflicts();
-			if (b1.isSolved())
-			{
-				cout << "Solved" << endl;
-			}
-			else
-			{
-				cout << "Not Solved" << endl;
-			}
+			system("pause");
+			solve(b1);
 		}
+		cout << "Total recursive calls: " << b1.getRecursiveCalls() << endl;
 		system("pause");
 	}
 	catch (indexRangeError &ex)
--- board.h
+++ board.h
@@ -13,19 +13,24 @@ public:
 	void clear();
 	void initialize(ifstream &fin);
 	void print();
-	void updateConflicts();
+	void updateConflicts(int, int, ValueType);
 	void printConflicts();
 	bool isBlank(int, int);
 	ValueType getCell(int, int);
 	void setCell(int, int, int);
-	void resetCell(int, int);
+	void resetCell(int, int, ValueType);
+	long getRecursiveCalls();
+	void incrementRecursiveCalls();
 	bool isSolved();
-
+	bool isLegal(int, int, ValueType);
+	void findMostConstrainedBlankCell(int&, int&);
+	void findNextBlankCell(int&, int&);
+	matrix<ValueType> getValue();
 private:
 
 	// The following matrices go from 1 to BoardSize in each
 	// dimension, i.e., they are each (BoardSize+1) * (BoardSize+1)
-
+	long numRecursiveCalls = 0;
 	matrix<ValueType> value;
 	matrix<bool> row_conf;
 	matrix<bool> col_conf;
